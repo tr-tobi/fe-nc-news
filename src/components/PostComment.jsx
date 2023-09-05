6f76baa175740fa4ac4f6df8c00b0b7959a0dceb
Tobi Thomas <tobi@tobis-air.home>
2023-09-05 17:57:09 +0100

    allows user to post comments to an article

---
+++ src/components/PostComment.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { postCommentByArticleId } from "../../utils/apicalls";
+
+const PostComment = ({ article_id, comments, setComments }) => {
+  const [comment, setComment] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isPosted, setIsPosted] = useState(false);
+
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsDisabled(true);
+    setIsPosted(true);
+    postCommentByArticleId(article_id, comment).then((newComment) => {
+      setComments((currentComments) => {
+        return [newComment.comment, ...currentComments];
+      });
+    });
+    setComment(comment);
+  };
+
+  return (
+    <div className="container-padding">
+      <form className="comment-card" onChange={handleChange}>
+        <p className="label">Post a Comment</p>
+        <textarea
+          id="comment"
+          cols="30"
+          rows="10"
+          placeholder="Type comment here"
+        />
+        <button
+          value={isPosted ? "" : comment}
+          onClick={handleSubmit}
+          className="center-button"
+          disabled={isDisabled ? true : false}
+        >
+          Post Comment
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default PostComment;
