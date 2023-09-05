925b93d182dd9e0de81dbc7b915f9e4616d3d95f
Tobi Thomas <tobi@tobis-air.home>
2023-09-05 14:44:22 +0100

    handles article votes

--- src/components/IndividualArticle.jsx
+++ src/components/IndividualArticle.jsx
@@ -1,21 +1,57 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getArticleById, getCommentByArticleId } from "../../utils/apicalls";
+import {
+  getArticleById,
+  getCommentByArticleId,
+  patchArticleVote,
+} from "../../utils/apicalls";
 import CommentCard from "./CommentCard";
-import { dateTimeSeperator } from "../../utils/function";
-
 const IndividualArticle = () => {
   const [article, setArticle] = useState({});
   const { article_id } = useParams();
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [incrIsDisabled, setIncrIsDisabled] = useState(false);
+  const [decrIsDisabled, setDecrIsDisabled] = useState(false);
+  const [upVoteValue, setUpVoteValue] = useState(1);
+  const [downVoteValue, setDownVoteValue] = useState(-1);
+  const [votes, setVotes] = useState(0);
 
+  const handleIncr = (event) => {
+    event.preventDefault();
+
+    if (upVoteValue === 1) {
+      setUpVoteValue(-1);
+      setDecrIsDisabled(true);
+    } else {
+      setUpVoteValue(1);
+      setDecrIsDisabled(false);
+    }
+    patchArticleVote(event.target.value, article_id).then(({ article }) => {
+      setVotes(article.votes);
+    });
+  };
+
+  const handleDecr = (event) => {
+    event.preventDefault();
+
+    if (downVoteValue === -1) {
+      setDownVoteValue(1);
+      setIncrIsDisabled(true);
+    } else {
+      setDownVoteValue(-1);
+      setIncrIsDisabled(false);
+    }
+    patchArticleVote(event.target.value, article_id).then(({ article }) => {
+      setVotes(article.votes);
+    });
+  };
   useEffect(() => {
     setIsLoading(true);
-
     getArticleById(article_id)
       .then((article) => {
         setArticle(article);
+        setVotes(article.votes);
       })
       .catch((err) => {
         setIsLoading(false);
@@ -48,9 +84,23 @@ const IndividualArticle = () => {
           <p>Date Posted: {article.created_at}</p>
           <p>Number of Comments: {article.comment_count}</p>
         </aside>
-        <button>Upvote</button>
-        <p>Votes: {article.votes}</p>
-        <button>DownVote</button>
+        <div>
+          <button
+            onClick={handleIncr}
+            disabled={incrIsDisabled ? true : false}
+            value={upVoteValue}
+          >
+            Upvote
+          </button>
+          <p>Votes: {votes}</p>
+          <button
+            onClick={handleDecr}
+            disabled={decrIsDisabled ? true : false}
+            value={downVoteValue}
+          >
+            DownVote
+          </button>
+        </div>
       </div>
       <h2>Comments</h2>
       {comments.length === 0 ? (
